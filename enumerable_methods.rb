7d7f97952f8b3051c7f53b64515679954c0a2d1b
vzdrizhni <vzdrizhni@gmail.com>
2020-03-05 17:57:33 +0500

    Added my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,7 +1,17 @@
 module Enumerable
-    def my_each
-        self.length.times do |i|
-          yield self[i] if block_given?
-        end
+
+  def my_each
+    length.times do |i|
+      yield self[i] if block_given?
     end
+  end
+
+  def my_each_with_index
+    length.times do |i|
+        yield self[i], i if block_given?
+    end
+  end
+
+  [2, 3, 4].my_each_with_index { |x, y| puts "#{x * 2} #{y + 1}" }
+
 end
