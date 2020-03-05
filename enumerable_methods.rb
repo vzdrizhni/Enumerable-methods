2db35d53452f87eda86391f71423af972ff4b079
vzdrizhni <vzdrizhni@gmail.com>
2020-03-05 19:05:57 +0500

    Added my_select

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -12,6 +12,16 @@ module Enumerable
     end
   end
 
-  [2, 3, 4].my_each_with_index { |x, y| puts "#{x * 2} #{y + 1}" }
+  def my_select
+    new_array = []
+    self.length.times do |i|
+        new_array << self[i] if yield self[i] 
+    end
+    new_array
+  end
+
+  #[2, 3, 4].my_each_with_index { |x, y| puts "#{x * 2} #{y + 1}" }
+
+ puts [2, 3, 4].my_select { |x| x > 2 }
 
 end
