bd48cff944f384f7bef5bcee4179e9e1cbd7e4ef
vzdrizhni <vzdrizhni@gmail.com>
2020-03-09 18:34:33 +0500

    Added Enumerator return if block is not given

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,23 +1,26 @@
 module Enumerable
   def my_each
-    self.length.times do |i|
-      yield self[i] if block_given?
+    return enum_for(:each) unless block_given?
+    self.size.times do |i|      
+      yield self.to_a[i]      
     end
-  end
+  end  
 
-  def my_each_with_index    
+  def my_each_with_index
+    return enum_for(:each_with_index) unless block_given?
     length.times do |i|
-      yield self[i], i if block_given?
+      yield self.to_a[i], i if block_given?
     end    
   end  
 
   def my_select
+    return enum_for(:select) unless block_given?
     new_array = []
     my_each do |value|
       new_array << value if yield value
     end
     new_array
-  end
+  end  
 
   def my_all?
     is_all_true = true
@@ -52,24 +55,27 @@ module Enumerable
   end
 
   def my_map
+    return enum_for(:map) unless block_given?
     new_array = []
     my_each do |value|
       new_array << yield(value)
     end
     new_array
-  end
+  end 
+
+  #p [2, 4, 3].select
 
   def my_inject    
     arr = self.drop(1)
-    counter = self[0]
+    counter = self.to_a[0]
     i = 0    
     while i < arr.length do      
-      counter = yield(counter, arr[i])
+      counter = yield(counter, arr.to_a[i])
       i += 1
     end
     counter
   end 
 
-  puts [2, 3, 4].my_inject { |x, y| x + y } 
+  puts (2..4).my_inject { |x, y| x * y } 
 
 end
